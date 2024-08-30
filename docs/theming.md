eb62797a41111d403df1f6748d2c928872fcd1b9
Jack Belmont <jackbelmont8@gmail.com>
2024-08-30 17:55:02 -0600

    Add theming

---
+++ docs/theming.md
@@ -0,0 +1,13 @@
+# Theming
+
+This folder goes over the riders-link application colors
+
+## Colors
+
+### Primary Color
+
+ <img src="./assets/PrimaryColor.PNG">
+
+### Material Design Theme Creation
+
+<img src="./assets/MaterialDesignThemeInput.PNG">
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,53 @@
 import { Stack } from "expo-router";
-import { PaperProvider } from "react-native-paper";
+import {
+  MD3DarkTheme,
+  MD3LightTheme,
+  PaperProvider,
+  adaptNavigationTheme,
+} from "react-native-paper";
 import { AuthSessionProvider } from "../wrappers/AuthSessionProvider";
+import { useColorScheme } from "react-native";
+import { lightThemeColors } from "@/constants/theming/light-theme-colors";
+import { darkThemeColors } from "@/constants/theming/dark-theme-colors";
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationLightTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import merge from "deepmerge";
+
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: NavigationLightTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
+
+const customLightTheme = {
+  ...MD3LightTheme,
+  colors: lightThemeColors,
+};
+
+const customDarkTheme = {
+  ...MD3DarkTheme,
+  colors: darkThemeColors,
+};
+
+const appliedDarkTheme = merge(DarkTheme, customDarkTheme);
+const appliedLightTheme = merge(LightTheme, customLightTheme);
 
 export default function RootLayout() {
+  const systemColorScheme = useColorScheme();
+
+  const paperTheme =
+    systemColorScheme === "dark" ? appliedDarkTheme : appliedLightTheme;
   return (
     <AuthSessionProvider>
-      <PaperProvider>
-        <Stack>
-          <Stack.Screen name="(app)" options={{ headerShown: false }} />
-          <Stack.Screen name="sign-in" options={{ headerShown: false }} />
-        </Stack>
+      <PaperProvider theme={paperTheme}>
+        <ThemeProvider value={paperTheme}>
+          <Stack>
+            <Stack.Screen name="(app)" options={{ headerShown: false }} />
+            <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+          </Stack>
+        </ThemeProvider>
       </PaperProvider>
     </AuthSessionProvider>
   );
