ea41cb671897d98f01bf85fd9b1d764dc1019fba
Jack Belmont <jackbelmont8@gmail.com>
2024-08-30 18:33:56 -0600

    cleanup login styles

--- src/app/sign-in.tsx
+++ src/app/sign-in.tsx
@@ -16,30 +16,41 @@ export default function SignInView() {
     <View
       style={{
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
+        justifyContent: "space-between",
+        paddingHorizontal: 16,
+        paddingVertical: 32,
       }}
     >
-      <View style={{ width: "80%", maxWidth: 400 }}>
-        <HelperText type="info">Sign in or create an account.</HelperText>
-        <TextInput
-          label="Username/Email"
-          value={userName}
-          onChangeText={(text) => setUserName(text)}
-          style={{ marginBottom: 16 }}
-        />
-        <TextInput
-          label="Password"
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-          style={{ marginBottom: 16 }}
-          secureTextEntry
-        />
-        <Button mode="contained" onPress={handleSignIn}>
-          Sign In
-        </Button>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <View style={{ width: "80%", maxWidth: 400 }}>
+          <HelperText type="info">Sign in or create an account.</HelperText>
+          <TextInput
+            label="Username/Email"
+            value={userName}
+            onChangeText={(text) => setUserName(text)}
+            mode="outlined"
+            style={{ marginBottom: 16 }}
+          />
+          <TextInput
+            label="Password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            style={{ marginBottom: 16 }}
+            mode="outlined"
+            secureTextEntry
+          />
+          <Button mode="contained" onPress={handleSignIn}>
+            Sign In
+          </Button>
+        </View>
       </View>
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <Text>Don't have an account?</Text>
         <Button onPress={() => console.log("create account")}>
           Create account.
